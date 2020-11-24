2aba9558b84c42f0437b66d84c02ad6bdcaf5c33
Mitchell Blom <mitchellblom@gmail.com>
2020-11-23 20:11:56 -0700

    datetime to date and time

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -6,7 +6,8 @@ class Event(models.Model):
     game = models.ForeignKey("Game", on_delete=CASCADE)
     organizer = models.ForeignKey("Gamer", on_delete=CASCADE, related_name="events", related_query_name="event")
     description = models.TextField()
-    datetime = models.DateTimeField()
+    date = models.DateField(default="2021-08-14")
+    time = models.TimeField(default="19:30")
 
     participants = models.ManyToManyField(
         "Gamer", 
